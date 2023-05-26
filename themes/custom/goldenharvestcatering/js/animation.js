a7bb73a4d4ab35e93a4e44281c0e98d681439fd9
Vũ Phương Anh <vuphuonganh020497@gmail.com>
2023-05-25 20:07:03 -0700

    add menu template

--- themes/custom/goldenharvestcatering/js/animation.js
+++ themes/custom/goldenharvestcatering/js/animation.js
@@ -1,1 +1,17 @@
-AOS.init();
+function service_owl() {
+  jQuery(".owl-carousel").owlCarousel({
+    items: 4,
+    margin: 10,
+    dots: true,
+    autoPlay: 3000,
+    navigation: true,
+    responsive: {
+      500: { items: 1, dots: true, navigation: true },
+      700: { items: 2, dots: true, navigation: true },
+      900: { items: 4, dots: true, navigation: true },
+    },
+  });
+}
+jQuery(document).ready(function ($) {
+  service_owl();
+});
